9028eb761f04b66a98a585c336abebda2f4d9d0e
Mohamed Sobhy <mosobhyy74@gmail.com>
2022-05-15 16:40:28 +0200

    Add coding files

---
+++ preprocessing.py
@@ -0,0 +1,29 @@
+import re
+
+# Clean/Normalize Arabic Text
+def ar_preprocessing(text):
+    search = ["أ", "إ", "آ", "ة", "_", "-", "/", ".", "،", " و ", " يا ", '"', "ـ", "'", "ى", "\\", '\n', '\t',
+              '&quot;', '?', '؟', '!']
+    replace = ["ا", "ا", "ا", "ه", " ", " ", "", "", "", " و", " يا", "", "", "", "ي", "", ' ', ' ', ' ', ' ? ', ' ؟ ',
+               ' ! ']
+
+    # remove tashkeel
+    p_tashkeel = re.compile(r'[\u0617-\u061A\u064B-\u0652]')
+    text = re.sub(p_tashkeel, "", text)
+
+    # remove longation
+    p_longation = re.compile(r'(.)\1+')
+    subst = r"\1\1"
+    text = re.sub(p_longation, subst, text)
+
+    text = text.replace('وو', 'و')
+    text = text.replace('يي', 'ي')
+    text = text.replace('اا', 'ا')
+
+    for i in range(0, len(search)):
+        text = text.replace(search[i], replace[i])
+
+    # trim
+    text = text.strip()
+
+    return text
